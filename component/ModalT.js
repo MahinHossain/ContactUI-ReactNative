8f37bbf37bf9d34ca31b6f7fbb0d6d03ed39860a
Md. Mahin Hossain <intern67.it@akij.net>
2021-06-24 17:59:46 +0600

    sectio list added

--- component/ModalT.js
+++ component/ModalT.js
@@ -1,100 +1,53 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect, useRef, useState} from 'react';
 import * as Animatable from 'react-native-animatable';
 import {Text, View, TouchableWithoutFeedback, ScrollView} from 'react-native';
-import {useRef} from 'react';
-import {useEffect} from 'react';
+import SectionListContacts from 'react-native-sectionlist-contacts';
+import axios from 'axios';
+
 function ExampleView() {
+  let sectionList = useRef([]);
+  const [data, setdata] = useState([]);
   const handleViewRef = useRef({});
   const bounce = () => {};
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        setdata(res.data);
+      })
+      .catch(err => console.log(`err`, err));
+  }, []);
 
-  return (
-    <TouchableWithoutFeedback>
-      <View
-        // ref={handleViewRef}
-        style={{alignContent: 'center', marginTop: 50}}>
-        <Animatable.Text
-          animation="bounce"
-          duration={5000}
-          easing="ease-in-out-back"
-          style={{backgroundColor: 'coral'}}>
-          Bounce me!
-        </Animatable.Text>
-        {/* <Text style={{fontFamily: 'normal'}}> normal </Text>
-        <Text style={{fontFamily: 'notoserif'}}> notoserif </Text>
-        <Text style={{fontFamily: 'sans-serif'}}> sans-serif </Text>
-        <Text style={{fontFamily: 'sans-serif-light'}}> sans-serif-light </Text>
-        <Text style={{fontFamily: 'sans-serif-thin'}}> sans-serif-thin </Text>
-        <Text style={{fontFamily: 'sans-serif-condensed'}}>
-          sans-serif-condensed
-        </Text>
-        <Text style={{fontFamily: 'sans-serif-medium'}}>sans-serif-medium</Text>
-        <Text style={{fontFamily: 'serif'}}> serif </Text>
-        <Text style={{fontFamily: 'Roboto'}}> Roboto </Text>
-        <Text style={{fontFamily: 'monospace'}}> monospace </Text> */}
-        <ScrollView>
-          <Text style={{fontFamily: 'normal'}}> normal </Text>
-          <Text style={{fontFamily: 'notoserif'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif-light'}}> Helloht </Text>
-          <Text style={{fontFamily: 'sans-serif-thin'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif-condensed'}}> Hello</Text>
-          <Text style={{fontFamily: 'sans-serif-medium'}}> sHello </Text>
-          <Text style={{fontFamily: 'serif'}}> Hello </Text>
-          <Text style={{fontFamily: 'Roboto'}}> Hello </Text>
-          <Text style={{fontFamily: 'monospace'}}> Hello </Text>
-          <Text style={{fontFamily: 'normal'}}> normal </Text>
-          <Text style={{fontFamily: 'notoserif'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif-light'}}> Helloht </Text>
-          <Text style={{fontFamily: 'sans-serif-thin'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif-condensed'}}> Hello</Text>
-          <Text style={{fontFamily: 'sans-serif-medium'}}> sHello </Text>
-          <Text style={{fontFamily: 'serif'}}> Hello </Text>
-          <Text style={{fontFamily: 'Roboto'}}> Hello </Text>
-          <Text style={{fontFamily: 'monospace'}}> Hello </Text>
-          <Text style={{fontFamily: 'normal'}}> normal </Text>
-          <Text style={{fontFamily: 'notoserif'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif-light'}}> Helloht </Text>
-          <Text style={{fontFamily: 'sans-serif-thin'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif-condensed'}}> Hello</Text>
-          <Text style={{fontFamily: 'sans-serif-medium'}}> sHello </Text>
-          <Text style={{fontFamily: 'serif'}}> Hello </Text>
-          <Text style={{fontFamily: 'Roboto'}}> Hello </Text>
-          <Text style={{fontFamily: 'monospace'}}> Hello </Text>
-          <Text style={{fontFamily: 'normal'}}> normal </Text>
-          <Text style={{fontFamily: 'notoserif'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif-light'}}> Helloht </Text>
-          <Text style={{fontFamily: 'sans-serif-thin'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif-condensed'}}> Hello</Text>
-          <Text style={{fontFamily: 'sans-serif-medium'}}> sHello </Text>
-          <Text style={{fontFamily: 'serif'}}> Hello </Text>
-          <Text style={{fontFamily: 'Roboto'}}> Hello </Text>
-          <Text style={{fontFamily: 'monospace'}}> Hello </Text>
-          <Text style={{fontFamily: 'normal'}}> normal </Text>
-          <Text style={{fontFamily: 'notoserif'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif-light'}}> Helloht </Text>
-          <Text style={{fontFamily: 'sans-serif-thin'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif-condensed'}}> Hello</Text>
-          <Text style={{fontFamily: 'sans-serif-medium'}}> sHello </Text>
-          <Text style={{fontFamily: 'serif'}}> Hello </Text>
-          <Text style={{fontFamily: 'Roboto'}}> Hello </Text>
-          <Text style={{fontFamily: 'monospace'}}> Hello </Text>
-          <Text style={{fontFamily: 'normal'}}> normal </Text>
-          <Text style={{fontFamily: 'notoserif'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif-light'}}> Helloht </Text>
-          <Text style={{fontFamily: 'sans-serif-thin'}}> Hello </Text>
-          <Text style={{fontFamily: 'sans-serif-condensed'}}> Hello</Text>
-          <Text style={{fontFamily: 'sans-serif-medium'}}> sHello </Text>
-          <Text style={{fontFamily: 'serif'}}> Hello </Text>
-          <Text style={{fontFamily: 'Roboto'}}> Hello </Text>
-          <Text style={{fontFamily: 'monospace'}}> Hello </Text>
-        </ScrollView>
+  const _renderHeader = params => {
+    console.log(`{params.key}`, params.key);
+    return (
+      <View style={{backgroundColor: ''}}>
+        <Text>{params.key}</Text>
       </View>
-    </TouchableWithoutFeedback>
+    );
+  };
+  const _renderItem = (item, index, section) => {
+    // console.log('---custom-renderItem--', item, index, section);
+    return (
+      <View style={{backgroundColor: ''}}>
+        <Text>{item.name}</Text>
+      </View>
+    );
+  };
+  return (
+    <View style={{height: 700}}>
+      <SectionListContacts
+        style={{backgroundColor: ''}}
+        // ref={s => (sectionList = s)}
+        sectionListData={data}
+        // sectionHeight={150}
+        // showsVerticalScrollIndicator={true}
+        // initialNumToRender={data.length}
+        renderHeader={_renderHeader}
+        renderItem={_renderItem}
+        // otherAlphabet="#"
+      />
+    </View>
   );
 }
 
